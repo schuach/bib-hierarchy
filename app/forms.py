1db5415111a97b6216cc62af282f88846d80d901
Stefan Schuh <stefan.schuh@uni-graz.at>
2020-04-22 10:50:27 +0200

    add WIENB to dropdown

--- app/forms.py
+++ app/forms.py
@@ -19,6 +19,7 @@ class GetACForm(FlaskForm):
         ("43ACC_OEAW", "ÖAW"),
         ("43ACC_LBK", "Kärntner Landesbibliothek"),
         ("43ACC_FHB", "Fachhochschule Burgenland"),
+        ("43ACC_WIENB", "Wienbibliothek im Rathaus"),
         ]
     acnr = StringField("AC-Nummer: ", validators=[DataRequired()])
     institution_code = SelectField("Institution: ", choices=institutions)
