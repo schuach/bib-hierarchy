ecd9fc49d29b4623d04325cb48371a4960ba80b3
Stefan Schuh <stefan.schuh@uni-graz.at>
2020-08-06 07:56:52 +0200

    add ABPU

--- app/forms.py
+++ app/forms.py
@@ -27,6 +27,7 @@ class GetACForm(FlaskForm):
         ("43ACC_VUW", "Veterinärmedizinische Universität Wien"),
         ("43ACC_UGL", "Kunstuniversität Linz"),
         ("43ACC_DLI", "Diözesanbibliothek Linz"),
+        ("43ACC_ABP", "Anton Bruckner Privatuniversität"),
     ]
     acnr = StringField("AC-Nummer: ", validators=[DataRequired()])
     institution_code = SelectField("Institution: ", choices=institutions)
